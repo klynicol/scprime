b1449d63e2c4f5b12f0b0ecaef15794e566c0bf1
Mark Wickline <mwicklinedev@gmail.com>
2023-01-20 07:12:55 -0600

    added pricing bash script

---
+++ pricing.sh
@@ -0,0 +1,7 @@
+./spc host config maxcollateral 200SCP
+./spc host config collateralbudget 2KS
+./spc host config minstorageprice 3.5SCP
+./spc host config collateral 3.5SCP
+./spc host config maxduration 16Weeks
+./spc host config mindownloadbandwidthprice 0.7SCP
+./spc host config minuploadbandwidthprice 0.7SCP
